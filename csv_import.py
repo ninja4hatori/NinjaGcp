1c54a0ab9630510b2cee67b9bd8178b6bd0aaa35
ninja4hatori <ninja.hatori.team4@gmail.com>
2020-01-04 23:20:53 +0530

    csv file import python code

---
+++ csv_import.py
@@ -0,0 +1,19 @@
+from google.cloud import bigquery
+client = bigquery.Client()
+filename = 'E:\\gcp\\test.csv'
+dataset_id = 'test'
+table_id = 'table1'
+
+dataset_ref = client.dataset(dataset_id)
+table_ref = dataset_ref.table(table_id)
+job_config = bigquery.LoadJobConfig()
+job_config.source_format = bigquery.SourceFormat.CSV
+job_config.skip_leading_rows = 1
+job_config.autodetect = True
+
+with open(filename, "rb") as source_file:
+    job = client.load_table_from_file(source_file, table_ref, job_config=job_config)
+
+job.result()  # Waits for table load to complete.
+
+print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
