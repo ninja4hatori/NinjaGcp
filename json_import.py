37ba0816f8d3be11ad19ecab9a6eb69b8cc2a124
ninja4hatori <ninja.hatori.team4@gmail.com>
2020-01-10 01:54:03 +0530

    import file changes and new json file

--- json_import.py
+++ json_import.py
@@ -1,18 +1,20 @@
 from google.cloud import bigquery
 client = bigquery.Client()
-filename = 'C:\\ninja\\NinjaGcp\\Sample-employee-JSON-data.json'
+#filename = 'C:\\ninja\\NinjaGcp\\Sample-employee-JSON-data.json'
+filename = 'sample-employee-details.json'
+#filename = 'a.json'
 dataset_id = 'dataset_ninja'
 table_id = 'table_employee'
 
 dataset_ref = client.dataset(dataset_id)
 table_ref = dataset_ref.table(table_id)
 job_config = bigquery.LoadJobConfig()
-job_config.source_format = bigquery.SourceFormat.JSON
-
+job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+job_config.autodetect = True
 
 with open(filename, "rb") as source_file:
     job = client.load_table_from_file(source_file, table_ref, job_config=job_config)
 
 job.result()  # Waits for table load to complete.
 
-print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
+print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id)):wq!
